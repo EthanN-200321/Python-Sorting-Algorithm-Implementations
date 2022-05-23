cc53b64bd5a7f72b81795f13037912d383224937
Ethan N <74840292+EthanN-200321@users.noreply.github.com>
2022-05-23 13:22:23 -0600

    Implmeneted cocktail sort

---
+++ algorithms/cocktailsort.py
@@ -0,0 +1,23 @@
+def cocktailsort(data):
+    swp = True
+    end = len(data) - 1
+    start = 0
+
+    while swp == True:
+        swp = False
+        for i in range(start, end):  # Forward Iteration
+            if data[i] > data[i+1]:
+                data[i], data[i+1] = data[i+1], data[i]
+                swp = True
+        
+        if swp == False:  # Break if no swaps occur
+            break
+
+        for i in range(end-1, start-1, -1):  # Backward iteration
+            if data[i] > data[i+1]:
+                data[i], data[i+1] = data[i+1], data[i]
+                swp = True
+        start += 1
+            
+    return data
+
