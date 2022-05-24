ca35c58fccd6768fff8f98055b54f3989f0427c8
Ethan N <74840292+EthanN-200321@users.noreply.github.com>
2022-05-24 17:39:28 -0600

    Removed quicksort

--- algorithms/bubblesort.py
+++ algorithms/bubblesort.py
@@ -1,15 +1,8 @@
 def bubblesort(data):
-    l = len(data)
-    for i in range(l):
-        for j in range(l - i - 1):
+    for i in range(len(data)):
+        for j in range(len(data) - i - 1):
             yield data
             if data[j] > data[j+1]:
                 data[j], data[j+1] = data[j+1], data[j]
     yield data
-
-
-if __name__ == "__main__":
-    a = bubblesort([3, 2, 1])
-    for i in a:
-        print(i)
 
