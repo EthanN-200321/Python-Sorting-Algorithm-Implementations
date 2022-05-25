a149ddc958b68a3c0386ec4e77b7a9f5cf05f424
Ethan N <74840292+EthanN-200321@users.noreply.github.com>
2022-05-24 22:39:57 -0600

    Implemented merge sort

--- algorithms/mergesort.py
+++ algorithms/mergesort.py
@@ -1,2 +1,21 @@
 def mergesort(data):
+    if len(data) <= 1:
+        return data
+
+    mid = len(data) // 2
+    l = mergesort(data[mid:])
+    r = mergesort(data[:mid])
+
+    res = []
+    i, j = 0, 0  # Left index, right index
+    while j < len(r) and i < len(l):
+        if r[j] > l[i]:
+            res.append(l[j])
+            i += 1
+        else:
+            res.append(r[j])
+            j += 1
+    res.extend(r[j:])
+    res.extend(l[i:])
+    return res
 
