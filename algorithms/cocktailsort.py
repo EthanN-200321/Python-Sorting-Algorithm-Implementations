762af287329f51f01bf23b627a6812b93bd54cee
Ethan N <74840292+EthanN-200321@users.noreply.github.com>
2022-05-24 18:20:10 -0600

    Refactored cocktail sort for visualization

--- algorithms/cocktailsort.py
+++ algorithms/cocktailsort.py
@@ -3,22 +3,21 @@ def cocktailsort(data):
     end = len(data) - 1
     start = 0
 
-    while swp:
+    while swp:  # Loop until no swaps occur
         swp = False
         for i in range(start, end):  # Forward Iteration
+            yield data
             if data[i] > data[i+1]:
                 data[i], data[i+1] = data[i+1], data[i]
-                yield 
                 swp = True
         
         if swp == False:  # Break if no swaps occur
             break
 
         for i in range(end-1, start-1, -1):  # Backward iteration
+            yield data
             if data[i] > data[i+1]:
                 data[i], data[i+1] = data[i+1], data[i]
                 swp = True
         start += 1
-            
-    return data
 
