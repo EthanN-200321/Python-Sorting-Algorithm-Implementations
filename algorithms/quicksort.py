1bb4a698f6a645decab3e8bda37ae2f4113a868d
Ethan N <74840292+EthanN-200321@users.noreply.github.com>
2022-05-28 17:00:44 -0600

    Refactored quicksort to be consistent with other algorithms for visualization.

--- algorithms/quicksort.py
+++ algorithms/quicksort.py
@@ -1,37 +1,25 @@
-class QS:
-    def __init__(self, data, l, h):  # Run quicksort, store output for visualization in a file
-        self.output_file = open("./outputs/quicksort_output", 'a+')
-        self.output_file.truncate()
-
-        self._quicksort(data, l, h)
-        self.output_file.close()
-        
-
-    def _partition(self, data, l, h):
+def quicksort(data, l, h):
+    # Partitioning
+    if l < h:
         pivot = data[(h + l) // 2]
         i = l
         j = h
 
 
         while True:
-            print((pivot, data), file=self.output_file)
+            yield list(data)
             while data[i] < pivot:
                 i += 1
             while data[j] > pivot:
                 j -= 1
             if i >= j:
-                return j
+                pivotind = j
+                break
             data[i], data[j] = data[j], data[i]
 
-
-    def _quicksort(self, data, l, h):
-        if l < h:
-            pivotind = self._partition(data, l, h)
-            self._quicksort(data, l, pivotind)
-            self._quicksort(data, pivotind + 1, h)
-
-if __name__ == "__main__":
-    a = [6, 1, 2, 3, 4, 9, 8, 0, 7, 5]
-    QS(data=a, l=0, h=len(a) - 1)
-    
+        yield list(data)
+        
+        # Quicksort
+        yield from quicksort(data, l, pivotind)
+        yield from quicksort(data, pivotind + 1, h)
 
